335e7a552719a7a542ebcf33e69835216d39d19e
Khalidprithy <khalidprithy@gmail.com>
2022-02-17 20:37:43 +0600

    error condition added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-document.getElementById('income-input').addEventListener('mouseleave', function () {
+function currentBalance() {
     const incomeInput = document.getElementById('income-input');
     const currentIncomeInput = incomeInput.value;
-    if (currentIncomeInput > 0) {
-        const currentBalance = document.getElementById('new-balance');
-        currentBalance.innerText = currentIncomeInput;
-        // incomeInput.value = ''
-    }
+    return currentIncomeInput;
+}
 
-});
+
 
 // getting expense function 
 
@@ -17,8 +14,19 @@ function getExpenseValues(inputId) {
     const expenseInputAmount = parseFloat(expenseInputText);
     expenseInput.value = '';
     return expenseInputAmount;
+
 }
 
+document.getElementById('income-input').addEventListener('mouseleave', function () {
+    //update balance
+    const currentIncomeInput = currentBalance();
+    if (currentIncomeInput > 0) {
+        const currentBalance = document.getElementById('new-balance');
+        currentBalance.innerText = currentIncomeInput;
+        // incomeInput.value = ''
+
+    }
+});
 
 document.getElementById('calc-btn').addEventListener('click', function () {
     //rent input
@@ -30,22 +38,21 @@ document.getElementById('calc-btn').addEventListener('click', function () {
     //others input
     const othersInputAmount = getExpenseValues('others-input');
 
-    // update total expense
-    const totalExpanseOutput = document.getElementById('total-expense');
-
-    const newTotalExpense = rentInputAmount + foodInputAmount + othersInputAmount;
-    const currentTotalExpense = parseFloat(totalExpanseOutput.innerText) + parseFloat(newTotalExpense);
-    totalExpanseOutput.innerText = currentTotalExpense;
-
-    // update balance
     const currentBalance = document.getElementById('new-balance');
     const currentBalanceText = currentBalance.innerText;
-    // const currentBalanceText = updateBalance();
-    console.log(currentTotalExpense);
 
-    const updatedNewBalance = parseFloat(currentBalanceText) - newTotalExpense;
+    // update total expense
+    const totalExpanseOutput = document.getElementById('total-expense');
+    const newTotalExpense = rentInputAmount + foodInputAmount + othersInputAmount;
+    if (currentBalanceText > newTotalExpense) {
+        const currentTotalExpense = parseFloat(totalExpanseOutput.innerText) + parseFloat(newTotalExpense);
+        totalExpanseOutput.innerText = currentTotalExpense;
+        // update balance
 
-    currentBalance.innerText = updatedNewBalance;
+        const updatedNewBalance = parseFloat(currentBalanceText) - newTotalExpense;
+
+        currentBalance.innerText = updatedNewBalance;
+    }
 
 });
 
@@ -56,18 +63,23 @@ document.getElementById('saving-btn').addEventListener('click', function updateS
     const savingInputText = savingInput.value;
     const savingAmountNumber = parseFloat(savingInputText);
 
-    // balance 
-    const incomeInput = document.getElementById('income-input');
-    const currentIncomeInput = incomeInput.value
-    // saving calculation 
-    const savingAmount = parseFloat((currentIncomeInput) / 100) * savingAmountNumber;
-    const savingAmountOutput = document.getElementById('saving-amount');
-    savingAmountOutput.innerText = savingAmount;
-
-    // updating currentBalance
     const currentBalance = document.getElementById('new-balance');
     const currentBalanceText = currentBalance.innerText;
-    const remainingBalanceOutput = document.getElementById('remaining-balance');
-    const remainingBalance = parseFloat(currentBalanceText) - parseFloat(savingAmount);
-    remainingBalanceOutput.innerText = remainingBalance;
+
+    // balance 
+    const incomeInput = document.getElementById('income-input');
+    const currentIncomeInput = incomeInput.value;
+    // saving calculation 
+    const savingAmount = parseFloat((currentIncomeInput) / 100) * savingAmountNumber;
+    if (currentBalanceText > savingAmount) {
+        const savingAmountOutput = document.getElementById('saving-amount');
+        savingAmountOutput.innerText = savingAmount;
+
+        // updating currentBalance
+
+        const remainingBalanceOutput = document.getElementById('remaining-balance');
+        const remainingBalance = parseFloat(currentBalanceText) - parseFloat(savingAmount);
+        remainingBalanceOutput.innerText = remainingBalance;
+    }
+
 });
